bb8183edcc9017cec6bd1acc9830921b822ade72
Alex Romanov <alex@strataconsulting.com>
2016-06-15 04:44:13 -0700

    typo

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe 'devopsbox::default' do
   
-  # expect terraform to be installed
+  # expect packer to be installed
   describe command('/usr/local/bin/packer version') do
     its(:stdout) { should match /^Packer v0\.10\.1.*$/ }
   end
